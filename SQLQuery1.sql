d5097443bdfe18a967648551ae73c80651d352dd
IshikaS07 <166443686+IshikaS07@users.noreply.github.com>
2024-04-15 01:34:56 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,21 @@
+--INSERT INTO EmployeeDemographics VALUES
+--(1001, 'Jim', 'Harplet', 30, 'Male'),
+--(1002, 'Pam', 'Beasley', 30, 'Female'),
+--(1003, 'Sky', 'Hon', 30, 'Female'),
+--(1004, 'Pam', 'Beasly', 29, 'Female'),
+--(1005, 'Angela', 'Martin', 27, 'Female'),
+--(1006, 'Toby', 'Scott', 30, 'Male'),
+--(1007, 'Stanely', 'Hudson', 35, 'Male'),
+--(1008, 'Kevin', 'Malone', 30, 'Male'),
+--(1009, 'Matt', 'Donavan', 36, 'Male')
+
+INSERT INTO EmployeeSalary VALUES
+(1001, 'Salesman',45000),
+(1002, 'Receptionist', 36000),
+(1003, 'Salesman', 63000),
+(1004, 'Accountant',47000),
+(1005, 'HR',50000),
+(1006, 'Regional Manger',65000),
+(1007, 'Supplier Relations',41000),
+(1008, 'Salesman',45000),
+(1009, 'Accountant',42000)
